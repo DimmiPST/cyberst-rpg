874bc22f3bafb7409211a1938a103ece0d6077fd
DimmiPST <jorge.scgs@gmail.com>
2026-08-19 22:43:02 -0300

    Criadas 2 novas funçoes para prosseguir com o modulo boot_camp e deixa-lo mais organizado bootcamp2: Fim da introduçao ao sistema de combate ate introduçao ao boss final bootcamp3: Ainda em criaçao , luta contra o boss final e encerramento do modulo bootcamp

--- CYBERSTER_SISTEMAS/boot_camp.py
+++ CYBERSTER_SISTEMAS/boot_camp.py
@@ -76,7 +76,7 @@ def boot_camp():
     print(f"{AZUL}Voce:{RESET} bem... vou fazer oque esse tal de coronel disse ..nunca usei uma arma ,será que consigo? ")
     time.sleep(3)
     print(f"{AZUL}*Surge um Alvo na sua frente*")
-    time.sleep(2)
+    time.sleep(2.5)
     print(f"Voce:{RESET} um alvo? ok se prepare!!! EU VOU SAIR DESSE PROJETO MALDITO")
     time.sleep(4)
     Jogador_bootcamp["Item Atual"] = Jogador_bootcamp['Itens'][1] #atribuindo Pistola como Item Atual
@@ -116,8 +116,82 @@ def boot_camp():
                     input(f"{VERMELHO} [ERRO] selecione um indice valido")
         else:
             input(f"{VERMELHO} [ERRO] selecione uma opçao valida(press enter)")
+def boot_camp2():
+        limpar()
+        time.sleep(2)
+        print("_"*30)
+        print("ALVO DERROTADO".center(30))
+        print("""⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⡀⢀⠀⢠⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢠⢤⣀⠀⠀⠀⠈⣆⢧⠈⡆⢸⠀⠀⠀⢰⢡⠇⠀⠀⠀⠀⠀⡀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⢀⠀⠀⣯⢀⣨⠃⠀⠀⠀⠸⡜⣄⣣⢸⠀⠀⠀⡜⡌⠀⠀⠀⠀⢀⡜⡁⠀⠀⠀⠀⠀
+    ⠀⠀⠙⢮⡳⢄⠈⠁⠀⢠⠴⠍⣛⣚⣣⢳⢽⡀⣏⣲⣀⢧⡥⠤⠶⢤⣠⢎⠜⠁⠀⠀⠀⠀⠀
+    ⠀⠠⣀⠀⠙⢦⡑⢄⢀⣾⣧⡎⠁⠀⠙⡎⡇⡇⡇⠹⢪⣀⡓⣦⢀⣼⣵⠋⢀⠴⣊⠔⠁⠀⠀
+    ⠀⠀⠈⠑⢦⣀⠙⣲⣝⢭⡚⠃⠀⠀⠀⠸⠸⣹⠁⠀⠀⠀⠉⣹⣪⣎⡸⢞⡵⠊⠁⣀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠈⣷⢯⣨⠷⣝⠦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠵⣪⢶⣙⡤⠖⢉⣀⠤⠖⠂
+    ⠀⠀⠀⠀⠀⢀⡞⢠⠾⠓⢮⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢬⣺⡯⢕⢲⠉⣥⣀⡀⠀⠀
+    ⠀⠀⢀⡤⣀⢈⡷⠻⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠘⠀⢱⢾⠘⢇⢴⠁⠀⠀
+    ⠀⠀⢻⣀⡼⢘⣧⢀⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⢙⣞⠆⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠉⠀⢿⡀⠈⠧⡤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠇⣹⣦⠇⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠸⢤⡴⢺⡧⣴⡶⢗⡣⠀⡀⠀⠀⠀⡄⠀⢀⣄⠢⣔⡞⣤⠦⡇⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⣀⡤⣖⣯⡗⣪⢽⡻⣅⠀⣜⡜⠀⠀⠀⠸⡜⡌⣮⡣⡙⢗⢏⡽⠁⠰⡏⠙⡆⠀⠀
+    ⠀⠀⣒⡭⠖⣋⡥⣞⣿⡚⠉⠉⢉⢟⣞⣀⣀⣀⠐⢦⢵⠹⡍⢳⡝⢮⡷⢝⢦⡀⠉⠙⠁⠀⠀
+    ⠐⠊⢡⠴⠚⠕⠋⠹⣍⡉⠹⢧⢫⢯⣀⣄⣀⠈⣹⢯⣀⣧⢹⡉⠙⢦⠙⣄⠑⢌⠲⣄⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠘⠧⡴⣳⣃⣸⠦⠴⠖⢾⣥⠞⠛⠘⣆⢳⡀⠈⠳⡈⠳⡄⠁⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⢀⡜⡱⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡄⢣⠀⠀⠉⠀⠈⠂⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⢀⠞⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⡀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""")
+        print("_"*30)
+        time.sleep(1.5)
+        print(f"{AZUL} *Coronel chega ao campo de tiro*")
+        time.sleep(1.5)
+        print("Coronel: olha.. oque aconteceu com voce?? kkkk voce sempre foi horrivel soldado")
+        time.sleep(0.5)
+        print("Horrivel nao...HORROROSO PIOR QUE HORRIVEL KKK parece ate outra pessoa")
+        time.sleep(2)
+        print(f"Voce:{RESET} obrigado senhor!tem mais algo que eu possa fazer???")
+        time.sleep(1)
+        print(f"{AZUL}Coronel: bem agora que voce mencionou tem sim, eu preciso que...")
+        time.sleep(2)
+        print("*Um soldado entra gritando no campo de tiro*")
+        time.sleep(1)
+        print("Soldado: CORONEL CORONEL CORONEL!!!!!!")
+        time.sleep(0.5)
+        print("Coronel: Oque foi Soldado, que desespero é esse???")
+        time.sleep(2)
+        print("Soldado:Coronel.. É que o DENJ-3  foi infectado por um virus trojan remoto..")
+        print("E ele está derrotando todos nossos soldados... alem de esta indo pro centro de pesquisa..")
+        time.sleep(3)
+        print("Coronel: O DENJ-3? QUER ME EXPLICAR COMO NOSSO MELHOR MECHA FOI INFECTADO???")
+        print("E QUEM TA CONTROLANDO ELE??")
+        time.sleep(3)
+        print("Soldado:Eu nao sei quem ta controlando.. acho que foi infectado depois de receber uma atualizaçao")
+        time.sleep(2)
+        print("Coronel: oh meu Deus...bem talvez seja uma boa oportunidade..")
+        time.sleep(2)
+        print("Soldado: Oportunidade de que coronel??")
+        time.sleep(2)
+        print("Coronel: De testar Um brinquedinho novo..")
+        print(f"Coronel: Ei {Jogador_bootcamp['Nome']} hoje é seu dia de sorte, pegue isso")
+        print("*Coronel entrega uma katana De Pulsos eletromagneticos (Katana-EMP)*")
+        time.sleep(5)
+        if "K-EMP" not in nome_itens:
+            Jogador_bootcamp["Itens"].append({
+                "Nome":"Katana EMP",
+                "Dano": 75,
+                "MAG": 2,
+                "CAP": 2,
+                "Descrição": "Primeira Katana a usar pulsos Eletromagneticos feita pelo Exército ,\n     produzidas por: Laboratorio Militar"
+            })
+        with open("jogador.json","w",encoding="utf-8") as jogadorjson:
+            json.dump(Jogador_bootcamp,jogadorjson,indent=4,ensure_ascii=False)
+        print(f"Coronel:{Jogador_bootcamp['Nome']} preste atençao essa katana ta em fase beta entao tome cuidado ")
+        print("Agora va e tire esse virus do mecha talvez seja necessario zerar a vida dele,depois continuamos a coversa..")
+        time.sleep(3)
+        input(f"*{Jogador_bootcamp['Nome']} corre pelo boot_camp ate encontrar o mecha infectado*")
+def boot_camp3():
     limpar()
-    print("_"*30)
-    print("ALVO DERROTADO".center(30))
-    print("_"*30)
-boot_camp()
+    print("-"*30)
+    print("PROXIMO AO CENTRO DE PESQUISA".center(30))
+    print("-"*30)
+    print(f"Você:{RESET}Achei.. vem aqui seu pedaço de lata")
+    #em produçao
