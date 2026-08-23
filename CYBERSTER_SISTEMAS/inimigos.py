6b22963248ee091bcd078d140a63d3954192ff7e
DimmiPST <jorge.scgs@gmail.com>
2026-08-23 13:32:59 -0300

    Adicionado novo Moculo para guardar as informaçoes dos inimigos

---
+++ CYBERSTER_SISTEMAS/inimigos.py
@@ -0,0 +1,16 @@
+#modulo onde armazena as informaçoes dos inimigos
+def alvo_bootcamp():
+    alvo_dic = {"Nome":"Alvo de Treino",
+            "Vida":180,
+            "VidaMax":180,
+            "Dano":0
+                }
+    return alvo_dic
+
+def denj3():
+    denj3_dic = {"Nome":"Denj-3",
+            "Vida":330,
+            "VidaMax":330,
+            "Dano":7
+                 }
+    return denj3_dic
